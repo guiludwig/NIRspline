19dcb575eb7b48a87d379b380c4abf8a9c4e02fd
guiludwig <guilhermeludwig@gmail.com>
2015-05-08 14:31:04 -0500

    updated readme

--- R/nirSpline.R
+++ R/nirSpline.R
@@ -1,6 +1,6 @@
-#' nirSpline.default
+#' nirSpline
 #' 
-#' Default action for the nirSpline function
+#' Fits an aggregated functional data model to near-infrared spectroscopy data.
 #' 
 #' @param wavelengths 
 #' @param absorbance
@@ -27,49 +27,5 @@
 #'
 #' @seealso \code{\link{tecator}}
 #' @keywords Aggregated Functional Data Analysis
-nirSpline.default <- function(wavelengths, absorbance, concentration, norder = 4){
-  I <- dim(absorbance)[1] # Number of components
-  n <- dim(absorbance)[2] # Number of wavelength samples
-  C <- dim(concentration)[2] # Number of analytes
-  if(dim(concentration)[1] != I) stop("Provide complete concentration values 
-                                      for all absorbance curves.")
-  K <- n-norder
-  absbasis <- fda::create.bspline.basis(rangeval = range(wavelengths), 
-                                        norder = norder, nbasis = K)
-  B <- fda::getbasismatrix(wavelengths, basisobj = absbasis)
-  X <- c(as.numeric(t(absorbance)), rep(0,n))    
-  Y <- cbind(rep(1,I), concentration)
-  Y <- rbind(Y, c(0, rep(1,C)))
-  R <- fda::getbasispenalty(absbasis)
-  
-  D.s <- kronecker(Y,B)
-  DD.s <- crossprod(D.s,D.s) # efficient computation
-  crossy <- crossprod(D.s,X)
-  p <- dim(Y)[2]
-  N <- length(X)
-  
-  GCV.2 <- function(lambda, y = X, ycross = crossy, DD = DD.s, 
-                    D = D.s, P = p, RR = R){
-    H.t <- solve(DD + kronecker(diag(P), lambda*RR), DD) # efficient trace
-    trace <- sum(diag(H.t))
-    adjust <- D%*%solve(DD + kronecker(diag(P),lambda*RR), ycross)
-    gcv <- N*sum((y-adjust)^2)/(length(y)-trace)
-    return(gcv)
-  }
-  lambda <- optimize(GCV.2, c(10^(-5),10^5))$minimum
-  
-  coefficients <- solve(DD.s + kronecker(diag(p),lambda*R), crossprod(D.s, X))
-  betaMatrix <- matrix(coefficients, nrow = K)
-    for(c in 1:C){
-    betaMatrix[, c+1] <- betaMatrix[, c+1] + betaMatrix[, 1]
-  }
-  
-  fitted <- D.s%*%coefficients
-  fitted <- as.numeric(fitted[c(1:n-1-length(X))]) # remove zeros
-  fittedMatrix <- matrix(fitted, ncol = n, byrow = TRUE)
-  
-  ret <- list(betaMatrix = betaMatrix, fittedMatrix = fittedMatrix)
-  class(ret) <- "nirSpline"
-  
-  return(ret)
-}
+nirSpline <- function(wavelengths, absorbance, concentration, ...)
+UseMethod("nirSpline")
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# NIRspline
+# nirSpline
 R scripts that fit and predict aggregated spline models for Near-Infrared spectroscopy data. 
+
+## References
+
+[http://www.tandfonline.com/doi/abs/10.1080/02664763.2014.938224#preview](Dias, R., Garcia, N., Ludwig, G. and Saraiva, M. (2014) Aggregated functional data model for Near-Infrared Spectroscopy calibration and prediction, *Journal of Applied Statistics*)
